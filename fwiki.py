97d8a69cbcfa9c78cbe5b483cca3996409bf900e
lucasstanesa <defitl@yahoo.com>
2014-10-09 22:56:25 -0400

    Experimenting with bbcode. Template changes too

--- fwiki.py
+++ fwiki.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect, g, url_for
 import sqlite3 as sqlite
+import bbcode
+
 
 app = Flask(__name__)
 
@@ -10,22 +12,40 @@ def route_index():
 @app.route('/<title>')
 def route_article(title):
 	title = title.replace("_", " ")
-	db_init()
+	
+	if not db_init():
+		return redier_template('error.html', error='Internal Error', message='An unexpected error occurred'), 503
+
 	g.cursor.execute("SELECT * FROM articles WHERE title=?", [title])
 	article = g.cursor.fetchone()
 	if article != None:
+		g.parser = bbcode.Parser()
+		g.parser.add_simple_formatter('wiki', url_for('route_article', title="%(value)s"))
 		return render_template('article.html', title=title, content=article['content'])
 	else:
-		return redirect('/')
+		return render_template('error.html', error='404', message='The requested page was not found'), 404
 
 @app.route('/random')
 def route_random():
-	redirect('/')
+	db_init()
+	g.cursor.execute("SELECT title FROM articles WHERE title != 'Main Page' ORDER BY RANDOM( ) LIMIT 1")
+	title = g.cursor.fetchone()['title']
+
+	if title:
+		return redirect('/' + title.replace(" ", "_"))
+	else:
+		return redirect('/')
+
 
 def db_init():
-	g.dbh = sqlite.connect("wiki.db")
-	g.dbh.row_factory = sqlite.Row
-	g.cursor = g.dbh.cursor()
+	try:
+		g.dbh = sqlite.connect("wiki.db")
+		g.dbh.row_factory = sqlite.Row
+		g.cursor = g.dbh.cursor()
+	except Exception:
+		return False
+	finally:
+		return True
 
 def db_close():
 	g.dbh.commit()
