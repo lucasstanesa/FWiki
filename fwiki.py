3e49c62fee98fda590741c5c72718a98fce7760a
lucasstanesa <lucasstanesa@flextech-hs.org>
2014-10-20 00:00:09 -0400

    Changed from bbcode to markdown

--- fwiki.py
+++ fwiki.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, g, url_for
 from markupsafe import Markup, escape
 import sqlite3 as sqlite
-import bbcode
 import markdown
 
 #Local imports
@@ -22,9 +21,8 @@ def route_article(title):
 
 	db_query("SELECT * FROM articles WHERE title=?", [title])
 	article = db_fetchone()
-	db_close()
+	db_close()	
 	if article != None:
-		markdown_init()
 		return render_template('article.html', title=title, content=escape(article['content']))
 	else:
 		return render_template('article.html', title=title, content="There is currently no text on this page")
@@ -53,11 +51,11 @@ def route_do_edit():
 def route_random():
 	db_init()
 	db_query("SELECT title FROM articles WHERE title!='Main Page' ORDER BY RANDOM() LIMIT 1")
-	title = db_fetchone()['title']
+	row = db_fetchone()
 	db_close()
 
-	if title:
-		return redirect('/' + title.replace(" ", "_"))
+	if row != None:
+		return redirect('/' + row['title'].replace(" ", "_"))
 	else:
 		return redirect('/')
 
@@ -65,8 +63,12 @@ def route_random():
 def error(error, message):
 	return render_template('error.html', error=error, message=message)
 
-def markdown_init():
-	g.parser = markdown.Markdown(safe_mode='')
+def markdown_parse(content):
+	return markdown.markdown(content)
+
+@app.context_processor
+def util_processor():
+    return dict(markdown_parse=markdown_parse)
 
 def bbcode_article(tag_name, value, options, parent, context):
 	url = url_for('route_article', title=value)
