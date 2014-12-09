66198b00502b0ea63494f42ba6ff948d7256e798
lucasstanesa <defitl@yahoo.com>
2014-12-09 10:45:25 -0500

    When you forget commiting again

--- config.py
+++ config.py
@@ -1,21 +1,17 @@
 from fwiki import app
 
-#Should we run in debug mode?
-app.config['debug'] = True
+app.config.update(
+    'DEBUG'        = True,      #Debug mode
+    'title'        = 'FWiki',   #Wiki title
+    'logo_file'    = None,      #Wiki logo, string filename or None for no logo
+    'expose'       = True,      #Print "Powered by..." in the footer?
+    'pass'         = '1234',    #Janitorial password
+    'locked'       = True,      #Password protect article creation and editing
+    
+    'db_name'      = 'wiki.db',
+    's_db_title'   = 'Internal Error',
+    's_db_msg'     = 'An interal error occurred',
+    's_lock_title' = 'Locked',
+    's_lock_msg'   = 'Article creation and editing are currently disabled'
+)
 
-#The title of the wiki
-app.config['title'] = ''
-
-#The wiki logo
-app.config['logo_file'] = None
-
-#Sets if "Powered by ..." appears in the footer
-app.config['expose'] = True
-
-#Password required to edit articles
-app.config['edit_pass'] = '1234'
-
-#Database options
-app.config['db_name'] = 'wiki.db'
-app.config['db_err_title'] = 'Interal Error'
-app.config['db_err_msg'] = 'An interal error occurred'
--- fwiki.py
+++ fwiki.py
@@ -12,76 +12,86 @@ import config
 @app.route('/', defaults={ 'title' : 'Main_Page' })
 @app.route('/<title>')
 def route_article(title):
-	title = title.replace("_", " ")
-	
-	if not database.init():
-		return error(app.config.db_err_title, config.db_err_msg), 503
+    title = title.replace('_', ' ')
+    
+    if not database.init():
+        return error(app.config['db_err_title'], app.config['db_err_msg']), 503
 
-	article =  database.fetch("SELECT * FROM articles WHERE title=?", [title])
-	database.close()	
-	if article != None:
-		return render_template('article.html', title=title, content=article['content'])
-	else:
-		return render_template('article.html', title=title, content="There is currently no text on this page")
+    article =  database.fetch('SELECT * FROM articles WHERE title = ? AND revision = 0', [title])
+    database.close()
+    if article != None:
+        return render_template('article.html', title=title, content=article['content'])
+    else:
+        return render_template('article.html', title=title, content='There is currently no text on this page')
 
 @app.route('/edit', defaults={ 'title' : None })
 @app.route('/edit/<title>')
 def route_edit(title):
-	if title == None:
-		return redirect('/')
-	title = title.replace("_", " ")
+    if title == None:
+        return redirect('/')
+    title = title.replace('_', ' ')
 
-	if not database.init():
-		return error(app.config.db_err_title, config.db_err_msg), 503
-	
-	article = database.fetch("SELECT * FROM articles WHERE title = ?", [title])
-	database.close()
+    if not database.init():
+        return error(app.config['s_db_title'], app.config['s_db_msg']), 503
+    
+    article = database.fetch('SELECT * FROM articles WHERE title = ?', [escape(title)])
+    database.close()
 
-	if article != None:
-		return render_template('edit.html', title=article['title'], id=article['id'], content=article['content'])
-	else:
-		return render_template('edit.html', title=title, id=0, content='')
+    if article != None:
+        return render_template('edit.html', title=article['title'], id=article['id'], content=article['content'])
+    else:
+        return render_template('edit.html', title=title, id=0, content='')
 
 @app.route('/do/edit', methods=['POST'])
 def route_do_edit():
-	title = request.form['title']
-	id = int(request.form['id'])
-	content = request.form['content']
+    title = form('title')
+    id = int(form('id'))
+    content = form('content')
+    hpot = form('email')
 
-	if app.config.edit_pass != None:
-		if request.form['pass'] != app.config.edit_pass:
-			return redirect('/')
+    if title is None or id is None or content is None or hpot is not "":
+        return 'Error'
 
-	if not database.init():
-		return error(app.config.db_err_title, config.db_err_msg), 503
+    if app.config['locked']:
+        if form('pass') != app.config['pass']:
+            return redirect('/')
 
-	if id == 0:
-		database.query("INSERT INTO articles VALUES(NULL, ?, ?)", [escape(title), escape(content)])
-	else:
-		database.query("UPDATE articles SET content = ? WHERE id = ?", [escape(content), id])
-	
-	database.close()
-	
-	return redirect(url_for('route_article', title=title))
+    if not database.init():
+        return error(app.config['db_err_title'], app.config['db_err_msg']), 503
+
+    if id == 0:
+        database.query('INSERT INTO articles VALUES(NULL, ?, ?, 0)', [escape(title), escape(content)])
+    else:
+        database.query("UPDATE articles SET revision = 1 WHERE title=?", [title])
+        database.query("INSERT INTO articles VALUES(NULL, ?, ?, 0)", [escape(title), escape(content)])
+
+    database.close()
+
+    return redirect(url_for('route_article', title=title))
 
 @app.route('/random')
 def route_random():
-	database.init()
-	row = database.fetch("SELECT title FROM articles WHERE title!='Main Page' ORDER BY RANDOM() LIMIT 1")
-	database.close()
+    database.init()
+    row = database.fetch("SELECT title FROM articles WHERE title!='Main Page' ORDER BY RANDOM() LIMIT 1")
+    database.close()
 
-	if row != None:
-		title = row['title'].replace(" ", "_")
-		return redirect('/' + title)
-	else:
-		return redirect('/')
+    if row != None:
+        title = row['title'].replace(' ', '_')
+        return redirect('/' + title)
+    else:
+        return redirect('/')
 
+def form(name):
+    if not name in request.form:
+        return None
+
+    return request.form[name]
 
 def error(error, message):
-	return render_template('error.html', error=error, message=message)
+    return render_template('error.html', error=error, message=message)
 
 def static_url(file):
-	return url_for('static', filename=file)
+    return url_for('static', filename=file)
 
 @app.context_processor
 def util_processor():
